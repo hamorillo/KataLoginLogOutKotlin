2daa690beb181692bc713a102a67f82fe62b9942
Héctor Abraham Morillo Prieto <hectorabrahammp@gmail.com>
2018-11-14 22:00:19 +0100

    Movement 1 done
    Added all the functionality specified on the “Movement 1” of the Readme
    We can make a login and logout with the special conditions defined

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ Kata from: https://github.com/Karumi/KataLogInLogOutKotlin/
 
 ## Movement 1: Writing the app.
 
-- [ ] Build an app with only one screen that allows you to log in if the username and the password are equal to admin.
-- [ ] After the log in, the user should be able to log out if the log out button is pressed and the second when the button was pressed was even.
-- [ ] Add validation for the username field so chars as ,, ., and ; are not allowed. Show a toast explaining the error if the username contain these chars.
-- [ ] The code to log the user in and out has to been executed out of the main thread.
+- [X] Build an app with only one screen that allows you to log in if the username and the password are equal to admin.
+- [X] After the log in, the user should be able to log out if the log out button is pressed and the second when the button was pressed was even.
+- [X] Add validation for the username field so chars as ,, ., and ; are not allowed. Show a toast explaining the error if the username contain these chars.
+- [X] The code to log the user in and out has to been executed out of the main thread.
 
 ## Movement 2: Testing the app using just unit tests.
 
--- app/src/main/java/com/hmorillo/kataloginlogoutkotlin/MainActivity.kt
+++ app/src/main/java/com/hmorillo/kataloginlogoutkotlin/MainActivity.kt
@@ -2,11 +2,53 @@ package com.hmorillo.kataloginlogoutkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import org.joda.time.DateTime
 
 class MainActivity : AppCompatActivity() {
+
+    private var logged = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViewActions()
+    }
+
+    private fun bindViewActions() {
+        loginButton.setOnClickListener { loginWith(usernameEditText.text.toString(), passwordEditText.text.toString()) }
+        logoutButton.setOnClickListener { logout() }
+    }
+
+    private fun loginWith(user: String, password: String) {
+        if(!validateUsername(user)){
+            Toast.makeText(this, "The username can't contain ',' '.' or ';'", Toast.LENGTH_LONG).show()
+        }
+        logged = user.toLowerCase() == "admin" && password.toLowerCase() == "admin"
+        updateView()
+    }
+
+    private fun validateUsername(user: String): Boolean {
+        return !(user.contains(",")
+                || user.contains(".")
+                || user.contains(";"))
+    }
+
+    private fun logout() {
+        logged = DateTime.now().secondOfMinute().get() % 2 == 0
+        updateView()
+    }
+
+    private fun updateView() {
+        if (logged) {
+            loginConstraintLayout.visibility = View.GONE
+            logoutConstraintLayout.visibility = View.VISIBLE
+        } else {
+            logoutConstraintLayout.visibility = View.GONE
+            loginConstraintLayout.visibility = View.VISIBLE
+        }
     }
 }
